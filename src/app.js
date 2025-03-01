a26d7e292ffa12f334e791471de4c68a82ffb942
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-03-01 04:06:26 +0000

    Initial commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const divPrincipal = document.querySelector("#div-principal")
+
+let carta = document.createElement("div")
+carta.classList.add("carta");
+
+let simboloArriba = document.createElement("div")
+simboloArriba.classList.add("simboloArriba");
+
+
+let numero = document.createElement("div")
+numero.classList.add("numero");
+
+
+let simboloAbajo = document.createElement("div")
+simboloAbajo.classList.add("simboloAbajo");
+
+
+carta.append(simboloArriba, numero, simboloAbajo)
+
+divPrincipal.appendChild(carta)
+
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  const simbolos = ["♦","♥" ,"♠", "♣"];
+  const valorDeLaCarta = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+
+  let indexDeValorDeSimbolo = Math.floor(Math.random()*simbolos.length)
+  let indexDeValorDeCarta = Math.floor(Math.random()*valorDeLaCarta.length)
+
+  simboloAbajo.innerText=simbolos[indexDeValorDeSimbolo];
+  simboloArriba.innerText=simbolos[indexDeValorDeSimbolo];
+
+  numero.innerText=valorDeLaCarta[indexDeValorDeCarta];
+
 };
+
+
